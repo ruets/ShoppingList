52a5056012d6f0c0bd63834dadf7171439e83f87
ruets <44617530+ruets@users.noreply.github.com>
2023-08-10 22:51:35 +0200

    add save and import options to settings page

--- lib/src/components/Settings.dart
+++ lib/src/components/Settings.dart
@@ -4,14 +4,9 @@ import 'package:ShoppingList/src/components/Confirmation.dart';
 
 import '../model/Api.dart';
 
-class Settings extends StatefulWidget {
+class Settings extends StatelessWidget {
   const Settings({super.key});
 
-  @override
-  State<Settings> createState() => _SettingsState();
-}
-
-class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,37 +15,116 @@ class _SettingsState extends State<Settings> {
       ),
       body: ListView(
         children: <Widget>[
-          ListTile(
-            title: const Text('Vider la liste'),
-            onTap: () {
-              showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment supprimer tous les éléments de la liste ?', 'Annuler', 'Supprimer').then((value) {
-                if (value != null && value) {
-                  setState(() {
+          const Text('\nGestion des données',style: TextStyle(fontWeight: FontWeight.bold)),
+
+          Card(
+            color: Colors.red,
+            child: ListTile(
+              title: const Text('Vider la liste'),
+              onTap: () {
+                showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment supprimer tous les éléments de la liste ?', 'Annuler', 'Supprimer').then((value) {
+                  if (value != null && value) {
                     Api.clearItems();
-                  });
-                }
-              });
-            },
+                  }
+                });
+              },
+            ),
           ),
 
-          ListTile(
-            title: const Text('A propos'),
-            onTap: () {
-              showAboutDialog(
-                context: context,
-                applicationName: 'ShoppingList',
-                applicationVersion: '1.0.0',
-                applicationIcon: const Icon(Icons.shopping_cart),
-                children: const <Widget>[
-                  Text('Application de liste de courses\n\n'
-                      'Développée par Sébastien RUET'
-                  ),
-                ],
-              );
-            },
+          const Text('\nSauvegarde et restauration', style: TextStyle(fontWeight: FontWeight.bold)),
+
+          Card(
+            color: Colors.white60,
+            child: ListTile(
+              title: const Text('Sauvegarde vers un fichier'),
+              onTap: () {
+                showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment sauvegarder la liste ?', 'Annuler', 'Sauvegarder').then((value) {
+                  if (value != null && value) {
+                    // TODO: save to file
+                  }
+                });
+              },
+            ),
           ),
+
+          Card(
+            color: Colors.white60,
+            child: ListTile(
+              title: const Text('Importer depuis un fichier'),
+              onTap: () {
+                showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment sauvegarder la liste ?', 'Annuler', 'Sauvegarder').then((value) {
+                  if (value != null && value) {
+                    // TODO: import from file
+                  }
+                });
+              },
+            ),
+          ),
+
+          const Text(''),
+          Card(
+            color: Colors.white60,
+            child: ListTile(
+              title: const Text('Sauvegarde sur le cloud'),
+              onTap: () {
+                showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment sauvegarder la liste ?', 'Annuler', 'Sauvegarder').then((value) {
+                  if (value != null && value) {
+                    // TODO: save to cloud
+                  }
+                });
+              },
+            ),
+          ),
+
+          Card(
+            color: Colors.white60,
+            child: ListTile(
+              title: const Text('Restauration depuis le cloud'),
+              onTap: () {
+                showConfirmationDialog(context, 'Confirmation', 'Voulez-vous vraiment sauvegarder la liste ?', 'Annuler', 'Sauvegarder').then((value) {
+                  if (value != null && value) {
+                    // TODO: restore from cloud
+                  }
+                });
+              },
+            ),
+          ),
+
+          const Text('\nDivers', style: TextStyle(fontWeight: FontWeight.bold)),
+
+          Card(
+            color: Colors.white60,
+            child: ListTile(
+              title: const Text('A propos'),
+              onTap: () {
+                showAboutDialog(
+                  context: context,
+                  applicationName: 'ShoppingList',
+                  applicationVersion: '1.0.0',
+                  applicationIcon: const Icon(Icons.shopping_cart),
+                  children: const <Widget>[
+                    Text('Application de liste de courses\n\n'
+                        'Développée par Sébastien RUET'
+                    ),
+                  ],
+                );
+              },
+            ),
+          )
         ],
       ),
     );
   }
 }
+
+// stateless widget
+// class Settings extends StatelessWidget {
+//   const Settings({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar()
+//     );
+//   }
+// }
