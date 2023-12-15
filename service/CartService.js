dd0041e62587ab9e8d37ea1003f7b8e487cfebfc
ruets <sebastien.ruet@etu.univ-grenoble-alpes.fr>
2023-12-15 14:56:53 +0100

    feat(cart): add carts methods

--- service/CartService.js
+++ service/CartService.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var client = require('../prisma/client')
 
 /**
  * Delete all items in the cart for this shop
@@ -9,7 +10,16 @@
  **/
 exports.cartDELETE = function(shopId) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    client.item.deleteMany({
+      where: {
+        shopId: shopId,
+        inCart: true
+      }
+    }).then((cart) => {
+      resolve(cart);
+    }).catch((error) => {
+      reject(error);
+    })
   });
 }
 
@@ -22,7 +32,16 @@ exports.cartDELETE = function(shopId) {
  **/
 exports.cartGET = function(shopId) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    client.item.findMany({
+      where: {
+        shopId: shopId,
+        inCart: true
+      }
+    }).then((cart) => {
+      resolve(cart);
+    }).catch((error) => {
+      reject(error);
+    })
   });
 }
 
@@ -36,7 +55,19 @@ exports.cartGET = function(shopId) {
  **/
 exports.cartPOST = function(body,shopId) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    client.item.create({
+      data: {
+        name: body.name,
+        quantity: body.quantity,
+        price: body.price,
+        inCart: true,
+        shopId: shopId,
+        categoryId: body.categoryId
+      }
+    }).then((shoppingList) => {
+      resolve(shoppingList);
+    }).catch((error) => {
+      reject(error);
+    })
   });
 }
-
